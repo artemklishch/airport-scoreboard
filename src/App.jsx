0a5fe3e7e0551267bf4f411d26acf5f7a07eeffe
Artem <artemklishch@gmail.com>
2020-03-08 13:38:47 +0200

    top form

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,12 @@ const App = () => {
       <Header />
       <main className='main'>
         <section className="main__top">
-        <p>fdsgfgdfdfg</p>
-        <p>fdsgfgdfdfg</p>
-        <p>fdsgfgdfdfg</p>
-        <p>fdsgfgdfdfg</p>
-        <p>fdsgfgdfdfg</p>
-        <p>fdsgfgdfdfg</p>
-        <p>fdsgfgdfdfg</p>
-        <p>fdsgfgdfdfg</p>
+          <h1 className="main__top_header">Flight search</h1>
+          <form action="GET" className="main__top_form">
+          <i className="fas fa-search main__top_form-glass"></i>
+          <input type="text" className="main__top_form-input" placeholder='Airline, destination or flight #' />
+          <button className="main__top_form-submit" type='submit'>Search</button>
+          </form>
         </section>
         
         </main>
