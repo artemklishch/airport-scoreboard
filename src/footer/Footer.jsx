a904c7712e269ecf28c803d22efde92d1907edb6
Artem <artemklishch@gmail.com>
2020-03-13 13:04:14 +0200

    added links

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 
 const Footer = () => {
   return (
@@ -7,31 +8,31 @@ const Footer = () => {
         <div className="footer__info-links__links-block">
           <h5 className="footer__info-links__links-block_title">For passengers</h5>
           <ul className="footer__info-links__links-block_list">
-            <li className="footer__info-links__links-block_item"><a href="#">Flight schedule</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Services</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Contacts</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Privacy policy</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Site map</a></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Flight schedule</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Services</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Contacts</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Privacy policy</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Site map</Link></li>
           </ul>
         </div>
         <div className="footer__info-links__links-block">
           <h5 className="footer__info-links__links-block_title">Corporate</h5>
           <ul className="footer__info-links__links-block_list">
-            <li className="footer__info-links__links-block_item"><a href="#">Main page</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Ground Handling</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Airport Characteristics</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Education center</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Careers</a></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Main page</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Ground Handling</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Airport Characteristics</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Education center</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Careers</Link></li>
           </ul>
         </div>
         <div className="footer__info-links__links-block">
           <h5 className="footer__info-links__links-block_title">Press centre</h5>
           <ul className="footer__info-links__links-block_list">
-            <li className="footer__info-links__links-block_item"><a href="#">Main page</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Latest news</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Social &#38 art projects</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Financial reports</a></li>
-            <li className="footer__info-links__links-block_item"><a href="#">Traffic statistics</a></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Main page</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Latest news</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Social &#38 art projects</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Financial reports</Link></li>
+            <li className="footer__info-links__links-block_item"><Link to='/'>Traffic statistics</Link></li>
           </ul>
         </div>
       </div>
@@ -40,15 +41,15 @@ const Footer = () => {
       <div className="footer__connect-links">
         <div className="footer__connect-links__phone">
           <h5 className="footer__connect-links__phone_title">Call information</h5>
-          <p><a href="#">+38 (044) 364-45-14</a></p>
+          <p><Link to='/'>+38 (044) 364-45-14</Link></p>
         </div>
         <div className="footer__connect-links__social-links">
         <h5 className="footer__connect-links__phone_title">Follow us</h5>
           <p>
-            <a href="#"><i className="fab fa-facebook-f"></i></a>
-            <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-instagram"></i></a>
-            <a href="#"><i className="fab fa-youtube"></i></a>
+            <Link to='/'><i className="fab fa-facebook-f"></i></Link>
+            <Link to='/'><i className="fab fa-twitter"></i></Link>
+            <Link to='/'><i className="fab fa-instagram"></i></Link>
+            <Link to='/'><i className="fab fa-youtube"></i></Link>
           </p>
         </div>
       </div>
@@ -58,11 +59,11 @@ const Footer = () => {
         <div className="footer__bottom-links_copyrights">
           <p>&#169; 2011-2020</p>
           <p>'Kyiv' International Airport</p>
-          <p><a href="#">Part of Ufuture Investment Group</a></p>
+          <p><Link to='/'>Part of Ufuture Investment Group</Link></p>
         </div>
         <div className="footer__bottom-links_developed">
           <p>Developed by</p>
-          <p><a href="#">smp/solutions</a></p>
+          <p><Link to='/'>smp/solutions</Link></p>
         </div>
       </div>
     </footer>
