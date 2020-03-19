515d6cc166a8c8c83ce9b153d50f5748d101eb6b
Artem <artemklishch@gmail.com>
2020-03-19 10:39:47 +0200

    f

--- src/main/main.gateway.js
+++ src/main/main.gateway.js
@@ -1,6 +1,8 @@
+import moment from 'moment';
+
 export const onGetDataAboutFlights = () => {
-  const todayDate = new Date().toLocaleDateString().split('.').join('-');
-  return fetch(`https://api.iev.aero/api/flights/${todayDate}`)
+  const todayDate = new Date();
+  return fetch(`https://api.iev.aero/api/flights/${moment(todayDate).format("DD-MM-YYYY")}`)
     .then(response => {
       if(response.ok) return response.json();
       throw new Error();
